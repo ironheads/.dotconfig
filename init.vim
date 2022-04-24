bae4cd0bee6dd5a66efa9e68a0fbef66ce988914
ironhead <iron_head@icloud.com>
2022-04-24 03:52:02 -0400

    :tada: ($root_dir): start the .dotfile config

---
+++ init.vim
@@ -0,0 +1,33 @@
+let s:curdir = fnamemodify(resolve(expand('<sfile>:p')), ':h')
+let &rtp.=','.s:curdir
+
+" Load configs
+for fpath in split(globpath(expand(s:curdir.'/config'), '*.vim'), '\n')
+  exe 'source' fpath
+endfor
+
+" Options setup ============================================================={{{
+
+
+set mouse=a
+set nocompatible
+" set hidden
+set encoding=UTF-8
+set termguicolors
+" Highlight curcur
+set cursorcolumn
+set cursorline
+" Max pattern match memory
+set maxmempattern=5000
+set lazyredraw
+set autoread
+set hlsearch
+set incsearch
+" set showmode
+set nobackup
+" set nowrap
+set number
+set relativenumber
+
+set ignorecase
+set smartcase
---
+++ load_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "start installing"
+VIMRC="${HOME}/.vimrc"
+CONFIG_DIR="${HOME}/.config"
+NVIM_CONFIG_DIR="${CONFIG_DIR}/nvim"
+INIT_VIM="init.vim"
+ROOT_DIR=$(pwd)
+# link vimrc
+if test -e "${VIMRC}"; then 
+    echo "$VIMRC exists, now delete it"
+    rm "$VIMRC"
+fi
+ln -s "${ROOT_DIR}/${INIT_VIM}" "$VIMRC"
+
+# link init.vim
+if [ ! -d "${CONFIG_DIR}" ]; then
+    echo "${CONFIG_DIR} does not exist, now create one"
+    mkdir "${CONFIG_DIR}"
+fi
+if [ ! -d "${NVIM_CONFIG_DIR}" ]; then
+    echo "${NVIM_CONFIG_DIR} does not exist, now create one"
+    mkdir "${NVIM_CONFIG_DIR}"
+fi
+if [ -e "${NVIM_CONFIG_DIR}/${INIT_VIM}" ]; then
+    echo "${NVIM_CONFIG_DIR}/${INIT_VIM} exists, now delete it"
+    rm "${NVIM_CONFIG_DIR}/${INIT_VIM}"
+fi
+ln -s "${VIMRC}" "${NVIM_CONFIG_DIR}/${INIT_VIM}"
+
